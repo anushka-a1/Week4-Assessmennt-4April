3739a14c72f2b0a958e21a71818b74fe447a9ab6
anushka-a1 <anushkachaurasia191@gmail.com>
2026-04-27 10:34:16 +0530

    Update run_tests.bat

--- run_tests.bat
+++ run_tests.bat
@@ -5,8 +5,8 @@ call .venv\Scripts\activate.bat
 if not exist reports mkdir reports
 
 pytest tests ^
- --html=reports\test_report.html ^
- --self-contained-html ^
- --junitxml=reports\junit_report.xml ^
- -v ^
- --tb=short
+--html=reports\test_report.html ^
+--self-contained-html ^
+--junitxml=reports\junit_report.xml ^
+-v ^
+--tb=short
